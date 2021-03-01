ae813f01abcc82dc7309d0a9b2c310eba7635f42
Matteo <matteo.ragazzini5@studio.unibo.it>
2021-03-01 12:36:24 +0100

    testIniatiallyEmpty

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,47 @@
+package lab01.tdd;
+
+import lab01.tdd.CircularList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    private List<Integer> list = new ArrayList<Integer>();
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    // TODO: 01/03/21
+
+    public void add(int element) {
+
+    }
+    // TODO: 01/03/21
+
+    public int size() {
+        return 0;
+    }
+
+    // TODO: 01/03/21
+    public Optional<Integer> next() {
+        return Optional.empty();
+    }
+
+    // TODO: 01/03/21
+    public Optional<Integer> previous() {
+        return Optional.empty();
+    }
+
+    // TODO: 01/03/21
+    public void reset() {
+
+    }
+
+    // TODO: 01/03/21
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,27 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        this.circularList = new CircularListImpl();
+    }
+
+    @Test
+    void testInitiallyEmpty(){
+        assertTrue(circularList.isEmpty());
     }
 
 }
